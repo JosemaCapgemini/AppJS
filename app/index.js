c0a26cdf8e2da158178f44691d4f35d391df5def
JosemaCapgemini <jose-manuel.rodriguez-rabadan@capgemini.com>
2018-11-15 14:21:09 +0100

    Event listener add

--- app/index.js
+++ app/index.js
@@ -1,5 +1,28 @@
-function test(){
-    document.getElementById("userName").innerHTML = "Josema"
+function changeUserName() {
+    document.getElementById("userName").innerHTML = "User"
 }
 
-test()
+function selectTag (tag) {
+
+    if (tag == 'tasks') {
+        document.getElementById("tasksId").style.backgroundColor = "000000"
+        document.getElementById("contactId").style.backgroundColor = "404040"
+    }
+    else if (tag == 'contact') {
+        document.getElementById("contactId").style.background = "000000"
+        document.getElementById("tasksId").style.backgroundColor = "404040"
+    }
+}
+window.onload = function() {
+    document.getElementById("tasksId").onclick = function fun() {
+        selectTag('tasks');
+    }
+
+    document.getElementById("contactId").onclick = function func() {
+        selectTag('contact');
+    }
+}
+
+changeUserName()
+
+
