49aa998a78e41ebaaf29a4b035d39ebfb5e40e72
JosemaCapgemini <jose-manuel.rodriguez-rabadan@capgemini.com>
2018-11-21 13:04:47 +0100

    Contact form added

--- build/dashboard.js
+++ build/dashboard.js
@@ -2,8 +2,6 @@ firebase.auth().onAuthStateChanged(function (user) {
     document.getElementById("userName").innerHTML = user['email']
 });
 
-
-
 function selectTag(tag) {
     if (tag == 'tasks') {
         document.getElementById("tasksId").style.backgroundColor = "black"
@@ -28,49 +26,56 @@ window.onload = function () {
 }
 
 function appendHTMLTasks() {
-    document.getElementById("bodyOn").innerHTML = "\
-        <button class='button buttonProjects'></button>"
-}
+    document.getElementById("buttonsId").innerHTML = "\
+    <button type='submit' class='button'>ADD PROJECT +</button>"
 
+    document.getElementById("bottomContainer").innerHTML = ""
+
+
+}
 
 function appendHTMLContact() {
-    document.getElementById("bodyOn").innerHTML = "\
-    <table>\
-        <tr>\
-            <th>Contact</th>\
-            <th>Favorite Color</th>\
-        </tr>\
-    </table>"
+    document.getElementById("buttonsId").innerHTML = ""
+
+    document.getElementById("bottomContainer").innerHTML = "\
+    <div class='container-contact100'>\
+		<div class='wrap-contact100'>\
+			<form class='contact100-form'>\
+				<span class='contact100-form-title'>Get in Touch</span>\
+				<div class='wrap-input100' data-validate='Name is required'>\
+					<input class='input100' id='name' type='text' name='name' placeholder='Name'>\
+					<label class='label-input100' for='name'>\
+						<span class='lnr lnr-user'></span>\
+					</label>\
+				</div>\
+				<div class='wrap-input100' data-validate='Valid email is required: ex@abc.xyz'>\
+					<input class='input100' id='email' type='text' name='email' placeholder='Email'>\
+					<label class='label-input100' for='email'>\
+						<span class='lnr lnr-envelope'><\span>\
+					</label>\
+				</div>\
+				<div class='wrap-input100' data-validate='Phone is required'>\
+					<input class='input100' id='phone' type='text' name='phone' placeholder='Phone'>\
+					<label class='label-input100' for='phone'>\
+						<span class='lnr lnr-phone-handset'></span>\
+					</label>\
+				</div>\
+				<div class='wrap-input100' data-validate='Message is required'>\
+					<textarea class='input100' name='message' placeholder='Your message...'></textarea>\
+				</div>\
+				<div class='contact100-form-checkbox'>\
+					<input class='input-checkbox100' id='ckb1' type='checkbox' name='copy-mail'>\
+					<label class='label-checkbox100' for='ckb1'>Send copy to my-email</label>\
+				</div>\
+				<div class='container-contact100-form-btn'>\
+					<div class='wrap-contact100-form-btn'>\
+						<div class='contact100-form-bgbtn'></div>\
+						<button class='contact100-form-btn'>Send Email</button>\
+					</div>\
+				</div>\
+			</form>\
+		</div>\
+	</div>"
 }
 
-
-// ------------------------------------------------------------------------------------------------------------
-
-var vue = new Vue({
-    el: '#app',
-    data: {
-      formOpen: false,
-      productData: {
-        title: '',
-        rating: '',
-        price: '',
-        list_price: '',
-        is_featured: false
-      }
-    },
-    methods: {
-      resetForm: function () {
-        this.productData = {
-          title: '',
-          rating: '',
-          price: '',
-          list_price: '',
-          is_featured: false
-        }
-      },
-      cancel: function() {
-        this.formOpen = false;
-        this.resetForm();
-      }
-    }
-  })
+    
